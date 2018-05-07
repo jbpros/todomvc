4effab34f0c83f7b9fd204e7dcc29a6fbd75c867
Julien Biezemans <jb@jbpros.com>
2018-05-07 10:56:19 +0200

    Set up Cucumber.js and Protractor

---
+++ examples/angularjs/features/step_definitions/todo_steps.js
@@ -0,0 +1,13 @@
+const { Given, When, Then } = require('cucumber')
+
+Given('things', function () {
+  return 'pending';
+});
+
+When('I do things', function () {
+  return 'pending';
+});
+
+Then('I should get things', function () {
+  return 'pending';
+});
---
+++ examples/angularjs/features/todo.feature
@@ -0,0 +1,6 @@
+Feature: TODO
+
+  Scenario:
+    Given things
+    When I do things
+    Then I should get things
